9fec8ffb9d6e5e0f10be38125bffe8536e719c2a
magmom95 <magmom95@naver.com>
2021-10-31 18:42:55 +0900

    plus comment

--- result/static/js/upload.js
+++ result/static/js/upload.js
@@ -1,15 +1,12 @@
 function readURL(input) {
   if (input.files && input.files[0]) {
-
+     // 인풋 태그에 파일이 있는 경우
     var reader = new FileReader();
 
     reader.onload = function(e) {
       $('.image-upload-wrap').hide();
-
-      $('.file-upload-image').attr('src', e.target.result);
+      $('.file-upload-image').attr('src', e.target.result);//기본적으로 이미지에 대한 경로를 미리 세팅 이거 그대로 모델링 할거니 경로를 결과값으로 (base64)
       $('.file-upload-content').show();
-
-      $('.image-title').html(input.files[0].name);
     };
 
     reader.readAsDataURL(input.files[0]);
@@ -19,19 +16,14 @@ function readURL(input) {
   }
 }
 
-function removeUpload() {
-  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
-  $('.file-upload-content').hide();
-  $('.image-upload-wrap').show();
-}
 $('.image-upload-wrap').bind('dragover', function () {
-		$('.image-upload-wrap').addClass('image-dropping');
-	});
-	$('.image-upload-wrap').bind('dragleave', function () {
-		$('.image-upload-wrap').removeClass('image-dropping');
+  $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function () {
+  $('.image-upload-wrap').removeClass('image-dropping');
 });
 
-function removeUpload(){
+function removeUpload() {
   $('.file-upload-input').replaceWith($('.file-upload-input').clone());
   $('.file-upload-content').hide();
   $('.image-upload-wrap').show();
